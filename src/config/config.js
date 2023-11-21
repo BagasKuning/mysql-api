03b9aafef0841d378c6868bfa5751560cdf1f01b
BagasKuning <dwibagaskara55@gmail.com>
2023-11-21 11:30:34 +0700

    set env

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ config()
 // create the connection to database
 export const dbPool = mysql.createConnection({
     host: process.env.DB_HOST,
-    user: 'root',
-    password: "",
-    database: 'express_mysql',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
